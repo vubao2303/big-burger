96b97a1c04d672f9eaa3563b3df9f21cbbf2bb21
BtramVu <bbbvut@gmail.com>
2021-02-01 22:23:33 -0800

    deploy to heroku

--- config/connection.js
+++ config/connection.js
@@ -28,28 +28,51 @@
 
 
 
-var mysql = require("mysql");
+// var mysql = require("mysql");
 
-// for local testing
-var connection = mysql.createConnection({
-  host: "localhost",
+// if (process.env.JAWSDB_URL) {
+//   connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+//   connection = mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password: "password",
+//     database: "burgers_db"
+//   });
+// }
+
+
+// // Make connection.
+// connection.connect(function (err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
+
+// // Export connection for our ORM to use.
+// module.exports = connection;
+
+
+// / Set up MySQL connection.
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host: process.env.host ||'localhost',
   port: 3306,
-  user: "root",
-  password: "password",
-  database: "burgers_db"
+  user: process.env.user || 'root',
+  // NOTE: Be sure to add your MySQL password here! we defaulted to : password
+  password: process.env.password || 'password',
+  database: process.env.db || "burgers_db",
 });
-
-// for Heroku
-// var connection = mysql.createConnection(process.env.JAWSDB_URL);
-
 // Make connection.
-connection.connect(function (err) {
+connection.connect((err) => {
   if (err) {
-    console.error("error connecting: " + err.stack);
+    console.error(`error connecting: ${err.stack}`);
     return;
   }
-  console.log("connected as id " + connection.threadId);
+  console.log(`connected as id ${connection.threadId}`);
 });
-
 // Export connection for our ORM to use.
 module.exports = connection;
